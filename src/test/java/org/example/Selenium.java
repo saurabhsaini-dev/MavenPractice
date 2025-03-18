318c41664427caf3ee325081018708956aae8311
saurabh7204 <saurabh.saini@delhivery.com>
2025-03-18 15:49:57 +0530

    fail

--- src/test/java/org/example/Selenium.java
+++ src/test/java/org/example/Selenium.java
@@ -1,10 +1,12 @@
 package org.example;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class Selenium {
     @Test
     public void browserAutomation() {
         System.out.println("browser");
+        Assert.fail();
     }
 }
